8dd334f33f4ed1db453c81591763c78404f76274
gaoxu <xu.gao@aorise.org>
2018-09-11 14:27:49 +0800

    修改mainActivity

--- app/src/main/java/com/android/frame/platform/MainActivity.java
+++ app/src/main/java/com/android/frame/platform/MainActivity.java
@@ -3,8 +3,8 @@ package com.android.frame.platform;
 import android.databinding.DataBindingUtil;
 
 import com.android.common.core.ui.base.BaseActivity;
-public class MainActivity extends BaseActivity {
 
+public class MainActivity extends BaseActivity {
 
 
     @Override
